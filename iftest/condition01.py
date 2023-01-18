f542c92b01f3cec77f1bae4b1c27d78cf578c49a
prabu-r84 <prabu.r@hpe.com>
2023-01-18 04:56:26 +0000

    While looping

--- iftest/condition01.py
+++ iftest/condition01.py
@@ -1,9 +1,9 @@
     #!/usr/bin/env python3
 
     # create the string hostname
-    hostname = ("MTG")
+    hostname = "MTG"
     # test logic with the `if` statement
     # what to do if this statement is found to be true
-    if hostname == "MTG":
+    if hostname = "MTG":
         print("The hostname was found to be MTG")
 
---
+++ lab70/slicingchallenge1.py
@@ -0,0 +1,4 @@
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+print("My " + str(challenge[2]) + "!" + "the " + str(challenge[2]) + "do " + str(challenge[3]) ) 
+print(f"My {str(challenge[2])} ! the {str(challenge[2])} do {str(challenge[3])}" ) 
+
