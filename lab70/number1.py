becbc65ec423f3f4b643c3e79a8735693ae2a3b8
prabu-r84 <prabu.r@hpe.com>
2023-01-18 10:11:53 +0000

    writing code with lists and dicts

---
+++ lab70/number1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Number guessing game! User has 5 chances to guess a number between 1 and 100!"""
+import random
+
+def main():
+    num= random.randint(1,100)
+
+    print(num)
+    guess= ""
+    rounds= 0
+
+    while rounds <= 5 and guess != num: 
+#     while rounds < 5 and print(num)
+        guess= input("Guess a number between 1 and 100\n>")
+
+        # COOL CODE ALERT: what is the purpose of the next fourlines?
+        if guess.isdigit():
+            guess= int(guess)
+        else:
+            continue
+
+        if guess > num:
+            print("Too high!")
+            rounds += 1
+
+
+        elif guess < num:
+            print("Too low!")
+            rounds += 1
+        else:
+            print("Correct!")
+
+        if rounds == 5:
+        answer = input(print(num))
+            print('the answer is :' + answer )
+
+main()
+
--- lab70/slicingchallenge1.py
+++ lab70/slicingchallenge1.py
@@ -1,4 +1,6 @@
 challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+eyes1= input(print(challenge[2]))
+print(f"My {str(eyes1[1])}")
 print("My " + str(challenge[2]) + "!" + "the " + str(challenge[2]) + "do " + str(challenge[3]) ) 
 print(f"My {str(challenge[2])} ! the {str(challenge[2])} do {str(challenge[3])}" ) 
 
