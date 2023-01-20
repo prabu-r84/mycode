f44e1d7b4cf5251d836ad1e6321efa60f4ea4f59
prabu-r84 <prabu.r@hpe.com>
2023-01-20 09:01:15 +0000

    Learning about classes

---
+++ smallscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Author: Prabu This program prints some things to the screen"""
+#the mynote setting the string value
+def main():
+    """Our main program"""
+    #create a simple string program
+    mynote = 'print this string on screan' # define mynote
+    print(mynote)                # display our string
+
+# call our main function
+main()
+
